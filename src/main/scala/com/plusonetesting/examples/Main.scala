a436e193112a1ce9a897f41874cf33bbdf38d0eb
Geir Gulbrandsen <dobbelg@gmail.com>
2018-10-24 21:09:50 +0100

    Implemented main method

--- src/main/scala/com/plusonetesting/examples/Main.scala
+++ src/main/scala/com/plusonetesting/examples/Main.scala
@@ -2,4 +2,16 @@ package com.plusonetesting.examples
 
 object Main extends App {
 
+  val persons = List(
+    Person("Akira", "Sakura", 12),
+    Person("Peter", "Muller", 34),
+    Person("Nick", "Tagart", 52)
+  )
+
+  val adults = Person.filterAdult(persons)
+
+  val descriptions = adults.map(p => p.description).mkString("\n\t")
+
+  println(s"The adults are \n\t$descriptions")
+
 }
