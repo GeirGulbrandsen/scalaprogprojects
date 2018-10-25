f8204145f6833940acdd34250f133d9eb492b601
Geir Gulbrandsen <ggulbrandsen@ebay.com>
2018-10-25 14:48:47 +0100

    add calculation of minimum months to save for retirement

--- src/main/scala/com/plusonetesting/retcalc/RetCalc.scala
+++ src/main/scala/com/plusonetesting/retcalc/RetCalc.scala
@@ -2,8 +2,21 @@ package com.plusonetesting.retcalc
 
 object RetCalc {
 
+  def futureCapital(interestRate: Double, nbOfMonths: Int, netIncome: Int,
+                    currentExpenses: Int, initialCapital: Double): Double = {
+
+    val monthlySavings = netIncome - currentExpenses
+
+    //    def nextCapital(accumulated: Double, month: Int): Double =
+    //      accumulated * (1 + interestRate) + monthlySavings
+
+    (0 until nbOfMonths).foldLeft(initialCapital)((accumulated, _) =>
+      accumulated * (1 + interestRate) + monthlySavings)
+  }
+
+
   def simulatePlan(interestRate: Double, nbOfMonthsSaving: Int, nbOfMonthsInRetirement: Int,
-                   netIncome: Int, currentExpenses: Int, initialCapital: Int) = {
+                   netIncome: Int, currentExpenses: Int, initialCapital: Int): (Double, Double) = {
 
     val monthlySavings = netIncome - currentExpenses
 
@@ -16,15 +29,23 @@ object RetCalc {
     (capitalAtRetirement, capitalAfterDeath)
   }
 
-  def futureCapital(interestRate: Double, nbOfMonths: Int, netIncome: Int,
-                    currentExpenses: Int, initialCapital: Double): Double = {
+  def nbOfMonthsSaving(interestRate: Double, nbOfMonthsInRetirement: Int, netIncome: Int,
+                       currentExpenses: Int, initialCapital: Int): Int = {
 
-    val monthlySavings = netIncome - currentExpenses
+    def loop(months: Int): Int = {
 
-//    def nextCapital(accumulated: Double, month: Int): Double =
-//      accumulated * (1 + interestRate) + monthlySavings
+      val (capitalAtRetirement, capitalAfterDeath) = simulatePlan(
+        interestRate, months, nbOfMonthsInRetirement, netIncome, currentExpenses, initialCapital)
 
-    (0 until nbOfMonths).foldLeft(initialCapital)((accumulated, _) =>
-      accumulated * (1 + interestRate) + monthlySavings)
+      val returnValue =
+        if (capitalAfterDeath > 0.0)
+          months
+        else
+          loop(months + 1)
+
+      returnValue
+    }
+
+    loop(0)
   }
 }
--- src/test/scala/com/plusonetesting/retcalc/RetCalcSpec.scala
+++ src/test/scala/com/plusonetesting/retcalc/RetCalcSpec.scala
@@ -37,8 +37,20 @@ class RetCalcSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
           nbOfMonthsSaving = 25 * 12, nbOfMonthsInRetirement = 40 * 12,
           netIncome = 3000, currentExpenses = 2000,
           initialCapital = 10000)
+
       capitalAtRetirement should === (541267.1990)
       capitalAfterDeath should === (309867.5316)
+    }
+  }
+
+  "RetCalc.nbOfMonthsSaving" should {
+    "calculate how long I need to save before I can retire" in {
+      val actual = RetCalc.nbOfMonthsSaving(
+        interestRate = 0.04/12, nbOfMonthsInRetirement = 40 * 12,
+        netIncome = 3000, currentExpenses = 2000, initialCapital = 10000)
+
+      val expexted = 23 * 12 + 1
+      actual should === (expexted)
     }
   }
 }
